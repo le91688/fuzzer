85d35461a194eefe69e862f6dbd65c5e16bed41a
Larry E <le91688@gmail.com>
2017-04-07 18:15:06 +0000

    up

--- fuzzy.py
+++ fuzzy.py
@@ -1,21 +1,18 @@
 import subprocess
 
 def run(data):
-    path = '/usr/bin/curl'
-    args = '--url'
+    path = '/somepath/to/something/'
+    args = '--somearg'
     try:
         ps = subprocess.check_output([path, args ,data])
         ps.wait()
     except Exception, e:
-        #print e.returncode
         if (e.returncode == -11):
             f.write ("overflow@:\n")
             f.write (data)
             f.write ("\n")
         else:
             pass
-#def makejunk():
-#    random.randint(1, 1000)
 
 if __name__ == "__main__":
     f = open('seglog', 'w') 
